a77be80764ca794b348f7637df28902eb742bd5c
LSBTrilho <leonardo.barbosa@cotrin.com.br>
2026-04-01 14:48:22 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import SecurityGuide from './components/SecurityGuide';
+
+export default function App() {
+  return (
+    <SecurityGuide />
+  );
+}
